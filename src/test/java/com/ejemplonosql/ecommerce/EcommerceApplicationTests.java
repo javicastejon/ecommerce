9d436f213f9c09a48aa1347e9ee2e9ec39f13fbf
jcastejon@e-itaca.es <jcastejon@e-itaca.es>
2026-04-23 11:38:49 +0200

    Add dev profile for test

--- src/test/java/com/ejemplonosql/ecommerce/EcommerceApplicationTests.java
+++ src/test/java/com/ejemplonosql/ecommerce/EcommerceApplicationTests.java
@@ -3,7 +3,7 @@ package com.ejemplonosql.ecommerce;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.profiles.active=dev")
 class EcommerceApplicationTests {
 
 	@Test
