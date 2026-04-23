06ba570845ec1e0fa65b321e366b198937036c10
jcastejon@e-itaca.es <jcastejon@e-itaca.es>
2026-04-23 11:56:50 +0200

    Check mock gemini service

--- src/test/java/com/ejemplonosql/ecommerce/EcommerceApplicationTests.java
+++ src/test/java/com/ejemplonosql/ecommerce/EcommerceApplicationTests.java
@@ -2,9 +2,16 @@ package com.ejemplonosql.ecommerce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import com.ejemplonosql.ecommerce.infrastructure.ai.GeminiAIService;
+
 
 @SpringBootTest(properties = "spring.profiles.active=dev")
 class EcommerceApplicationTests {
+
+    @MockitoBean
+    private GeminiAIService geminiAIService;
 
 	@Test
 	void contextLoads() {
